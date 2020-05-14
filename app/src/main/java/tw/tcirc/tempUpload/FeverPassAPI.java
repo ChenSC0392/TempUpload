6f1a50da0b3154f893fd4d6d1a7e8cbcf42cd2fc
nevikw39 <nevik.w39@gmail.com>
2020-05-14 23:38:04 +0800

    Comments

--- app/src/main/java/tw/tcirc/tempUpload/FeverPassAPI.java
+++ app/src/main/java/tw/tcirc/tempUpload/FeverPassAPI.java
@@ -1,3 +1,9 @@
+/**
+ * 05/14, 2020
+ *
+ * @author nevikw39
+ */
+
 package tw.tcirc.tempUpload;
 
 import android.content.Context;
@@ -15,19 +21,33 @@ import java.net.CookieManager;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FeverPassAPI {
+/**
+ * A simple API to submit daily body temperature to feverpass.life.
+ */
+class FeverPassAPI {
 
     private CookieManager cookieManager;
     private RequestQueue requestQueue;
 
+    /**
+     * Initialize the API instance with an {@code Context}.
+     * @param context An {@code android.content.Context} object, such as a MainActivity.
+     */
     FeverPassAPI(Context context) {
         cookieManager = new CookieManager();
         CookieHandler.setDefault(cookieManager);
         requestQueue = Volley.newRequestQueue(context);
     }
 
+    /**
+     * Make a post request based on Volley library.
+     * @param url The target url.
+     * @param param The form data.
+     * @param after An request which will be added to requestQueue after this request success. If it is null, then nothing happen.
+     * @return The {@code com.android.volley.toolbox.StringRequest} object.
+     */
     private StringRequest makePostRequest(String url, final Map<String, String> param, final StringRequest after) {
-        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+        return new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.d("FeverPassAPI.makePostRequest", response);
@@ -46,10 +66,16 @@ public class FeverPassAPI {
                 return param;
             }
         };
-        return request;
     }
 
-    public void submitTemp(final String username, final String password, final String temperature) {
+    /**
+     * Submit given body temperature.
+     *
+     * @param username    The student id which is used to login to feverpass.life
+     * @param password    The password.
+     * @param temperature The temperature in Celsius degree.
+     */
+    void submitTemp(final String username, final String password, final String temperature) {
         StringRequest submit = makePostRequest("https://tcfsh.feverpass.life/", new HashMap<String, String>() {{
             put("temperature", temperature);
             put("type", "2");
